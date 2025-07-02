fa5320b720bd5c270dd898dac36f6c494af5c66c
ahsan027 <ahsanauddry.ndc@gmail.com>
2025-07-03 00:40:00 +0600

    Pushes

--- Backend/server.js
+++ Backend/server.js
@@ -1,2 +1,14 @@
-console.log("Hello New user");
-console.log("Hi again");
+require("dotenv").config;
+
+const express = require("express");
+
+const connectDB = require("./config/database");
+
+const port = process.env.PORT || 5000;
+connectDB();
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server started Running at. ${port}`);
+});
